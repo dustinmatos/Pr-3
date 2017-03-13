5132206223350125a05098e2b82f306a895af4a9
dustinmatos <Dustin@Dustins-MBP.rlab.kpu.ca>
2017-03-13 15:35:47 -0700

    Ipad Test, Possible Final Commit
    Jquery added, back buttons and functions added, sync for testing.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
+$("#title").click(function() {
+  $("#nav").slideDown('fast');
+  $("#title").slideUp();
+});
 
+$("#history").click(function() {
+  $("#gallery").slideDown('fast');
+  $("#nav").slideUp();
+});
+
+$("#display").click(function() {
+  $("#display-page").slideDown('fast');
+  $("#nav").slideUp();
+});
+
+$(".back-btn").click(function() {
+  var historyIsShown = $("#gallery").css("display");
+  if (historyIsShown == "block") {
+    $("#nav").slideDown('fast');
+    $("#gallery").slideDown('fast');
+  } else {
+    $("#nav").slideDown('fast');
+    $("#display-page").slideDown('fast');
+  }
+});
